6a8a61a07c8384674975fb332822476aacf4b1b2
Nóra Máté <norama.matema@gmail.com>
2019-12-15 00:34:55 +0100

    Initial version. TODO: fix login hack

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ToastContainer, toast } from 'react-toastify';
+
+import Main from './Main';
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Main />
+        <ToastContainer position="bottom-center" autoCloe={8000} />
     </div>
-  );
-}
+);
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+
+import GoogleLogin from 'react-google-login';
+import GoogleButton from 'react-google-button';
+import { toast } from 'react-toastify';
+
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+import './Main.css';
+
+
+const GOOGLE_CLIENT_ID = "18175686909-9ruee794234g1q8qsc3rdps0flr3ddnp.apps.googleusercontent.com";
+
+const Main = () => {
+
+    const [ targetMapUrl, setTargetMapUrl ] = useState('');
+    const [ token, setToken ] = useState('');
+    const [ authorization, setAuthorization ] = useState(null);
+
+    const responseGoogle = (response) => {
+        console.log('accessToken', response.accessToken);
+
+        fetch(process.env.REACT_APP_MAPOTIC_API + '/auth/social/login/google/', {
+            method: "POST",
+            headers: { accept: 'application/json' },
+            body: { access_token: response.accessToken }
+        }).then((response) => {
+            if (!response.ok) {
+                throw response;
+            }
+            return response.json();
+        }).then((response) => {
+            console.log(response);
+            setAuthorization('Token ' + response.token)
+        }).catch((error) => {
+            toast.error('Error');
+            console.error(error);
+        }).finally(() => {
+        });
+    };
+
+    const failureGoogle = (error) => {
+        toast.error("Google login failure");
+        console.log(error);
+    }
+
+    const handleChange = (event) => {
+        setTargetMapUrl(event.target.value);
+    };
+
+    const handleTokenChange = (event) => {
+        setToken(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        setAuthorization(new String('Token ' + token));
+    };
+
+    useEffect(() => {
+        if (authorization) {
+            fetch(process.env.REACT_APP_MAPOTIC_API + '/auth/me/', {
+                method: "GET",
+                headers: { accept: 'application/json',  authorization },
+            }).then((response) => {
+                if (!response.ok) {
+                    throw response;
+                }
+                return response.json();
+            }).then((response) => {
+                console.log(response);
+                if (response.maps && response.maps.my) {
+                    if (response.maps.my.find(map => map.url === targetMapUrl)) {
+                        toast.success('Success');
+                    } else {
+                        toast.error('URL: ' + targetMapUrl + ' is not among user maps.');
+                    }
+                } else {
+                    toast.error('No maps for user.')
+                }
+            }).catch((error) => {
+                toast.error('Error');
+                console.error(error);
+            }).finally(() => {
+            });
+        }
+    }, [ authorization ]);
+
+    return (
+        <div className="__Main__">
+            <Form className="main-form">
+                <FormGroup>
+                    <Input type="url" name="targetMapUrl" value={targetMapUrl} onChange={handleChange} id="targetMapUrl" placeholder="target map URL" />
+                </FormGroup>
+                <FormGroup>
+                    <Input type="text" name="token" value={token} onChange={handleTokenChange} id="token" placeholder="Mapotic token copied from mapotic network" />
+{/*
+                    <GoogleLogin
+                        clientId={GOOGLE_CLIENT_ID}
+                        render={(renderProps) => (
+                            <GoogleButton onClick={renderProps.onClick} label="login with Google" />
+                        )}
+                        onSuccess={responseGoogle}
+                        onFailure={failureGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+*/}
+                </FormGroup>
+
+                <FormGroup>
+                    <Button onClick={handleSubmit}>Submit</Button>
+                </FormGroup>
+            </Form>
+        </div>
+    );
+};
+
+export default Main;
