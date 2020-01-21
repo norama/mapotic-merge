432c3511a49677f9fa0afa35a33a10e2d8a1b9b8
Nóra Máté <norama.matema@gmail.com>
2020-01-21 16:53:25 +0100

    Chrome ext: config with colors

--- src/mapotic/chrome/config.js
+++ src/mapotic/chrome/config.js
@@ -1,7 +1,7 @@
 const config = {
     collections: [{
         name: "swimming",
-        color: "ED57F9",
+        color: "9BAECC",
         sources: [{
             map: "https://www.mapotic.com/sauny"
         }, {
@@ -9,12 +9,14 @@ const config = {
         }]
     }, {
         name: "skiing",
+        color: "B4DADE",
         sources: [{
             map: "https://www.mapotic.com/winter-holidays-in-the-alps",
             categories: ["Ski center", "Glacier"]
         }]
     }, {
         name: "nature",
+        color: "B7DBB2",
         sources: [{
             map: "https://www.mapotic.com/chranena-uzemi"
         }, {
@@ -22,14 +24,22 @@ const config = {
         }]
     }, {
         name: "culture",
+        color: "E0C084",
         sources: [{
-            color: "E6F95C",
             map: "https://www.mapotic.com/praha-galerie"
         }, {
             map: "https://www.mapotic.com/festivals-europe"
+        }, {
+            map: "https://www.mapotic.com/kde-jsme-potkali-svjana-nepomuckeho"
+        }, {
+            map: "https://www.mapotic.com/ppp-patrame-po-pomukovi"
+        }, {
+            map: "https://www.mapotic.com/vozejkmap",
+            categories: ["Kultura"]
         }]
     }, {
         name: "drinking",
+        color: "F7A3BE",
         sources: [{
             map: "https://www.mapotic.com/wineofczechia",
             categories: ["Vinařství", "Na skleníčku Pro láhev", "Nevinářství", "Vše pro vino"]
@@ -40,6 +50,7 @@ const config = {
         }]
     }, {
         name: "shopping",
+        color: "ECF0A5",
         sources: [{
             map: "https://www.mapotic.com/smarty-drink"
         }]
